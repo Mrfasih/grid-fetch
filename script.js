663e12be86d7b41529c8ed07e891ce1b409113b4
Fasihzaib-1 <fasih.maltathemes@gmail.com>
2025-08-26 23:33:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+async function data(){
+let x = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const y= await x.json();
+    const grid = document.querySelector('.ok')
+            console.log(y);
+
+    y.forEach((posts)=>{
+        console.log(posts);
+        const div = document.createElement('div')
+        div.innerHTML=`<h1>ID:_${posts.id}</h1> <p>Title:${posts.title}</p> <p>Body:${posts.body}</p>`
+        grid.appendChild(div)
+        div.style.backgroundColor='rgba(224, 214, 204, 0.8)'
+        div.style.border='5px solid black'
+        div.style.padding='20px'
+        grid.style.display='grid'
+        grid.style.gridTemplateColumns='repeat(3,1fr)'
+        grid.style.gap='10px'   
+    })
+}
+console.log(data());
+
